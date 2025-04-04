81092e76954aeac83de2dad6fbd0b56d6a2c67a2
omoissy <omoissy@gmail.com>
2025-04-04 11:44:52 -0400

    Initial folder structure and script

---
+++ scripts/1_eda_cleaning.py
@@ -0,0 +1,10 @@
+# %% 
+import pandas as pd
+
+df = pd.read_excel("/Users/mac/OIL-GAS-PRODUCTIVITY-ANALYSIS/oil-gas-productivity-analysis/data/raw/all-years-states.xls")
+
+# Quick check
+print(df.head())
+print(df.info())
+
+# %%
